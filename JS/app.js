2c3ec44f338f8930f6bc92b9e02f6dcc989a6646
PabloOfFreedom <Misha2009mischuk@gmail.com>
2026-09-19 05:58:19 +0300

    Changed HTML/CSS to under different warm design

--- JS/app.js
+++ JS/app.js
@@ -1,5 +1,5 @@
 const STORAGE_KEY = "hotelbook_data_v1";
-
+const API_URL = "http://127.0.0.1:5050";
 const defaultData = {
   currentUser: null,
   users: [
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const fs = require('fs/promises');
 const path = require('path');
 
 const app = express();
-const PUBLIC_DIR = path.join(__dirname, 'public');
+const PUBLIC_DIR = path.join(__dirname);
 
 app.use(express.json());
 
